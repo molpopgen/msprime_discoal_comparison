70b892a511815ed53d522ce2a1c566afc72747ea
molpopogen <krthornt@uci.edu>
2021-07-09 15:45:15 -0700

    start on forward sims

--- compare.R
+++ compare.R
@@ -5,12 +5,15 @@ for (i in c(1, 100, 500, 1000))
 {
     mspfile = paste("msprime_", i, ".segsites", sep="")
     dscfile = paste("discoal_", i, ".segsites", sep="")
+    fp11file = paste("fp11_", i, ".segsites", sep="")
 
     m = scan(mspfile, quiet=TRUE)
     d = scan(dscfile, quiet=TRUE)
+    f = scan(fp11file, quiet=TRUE)
 
     plot(ecdf(m), main=paste("Initial freq = ", 0.5*i/1e4), xlim=c(2500,5000))
     lines(ecdf(d), col="blue")
+    lines(ecdf(f), col="magenta")
     lines(ecdf(mn), col="red")
 
     print(paste(i, 0.5*i/1e4, mean(m), mean(d), ks.test(m, d)$p.value))
